52391c51093f0660252c3041ca6f8a080bac5119
kennet10 <kennet10@uci.edu>
2022-01-26 20:59:43 -0800

    Basic Shop UI
    made basic shop UI with plants button and animals button, both open up a list of buttons which will be plants/animals

---
+++ New Unity Project/Assets/Scripts/Animal.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    [SerializeField] AnimalSO animalSO;
+
+    private int buy_cost;
+    private int sell_price;
+    private float productionCD;
+    private bool onCD;
+
+    private void Start()
+    {
+        buy_cost = animalSO.GetBuyCost();
+        sell_price = animalSO.GetSellPrice();
+        productionCD = animalSO.GetProductionCD();
+        onCD = false;
+    }
+
+    public void FeedAnimal()
+    {
+        StartCoroutine(GoOnCooldown());
+        //call function to increase product count by 1
+    }
+
+    private IEnumerator GoOnCooldown()
+    {
+        onCD = true;
+        yield return new WaitForSeconds(productionCD);
+        onCD = false;
+    }
+}
